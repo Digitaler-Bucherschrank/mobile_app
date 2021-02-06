6ca9a778f6c36059fc3e668441061e93e08b492d
Flawn <baran.peters@lessing-ffm.net>
2021-02-07 00:03:03 +0100

    display: Enable 90Hz natively for devices, that support (e.g. Oneplus 7 Pro)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 //import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'gmap.dart';
@@ -38,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     _getLocationPermission();
+    FlutterDisplayMode.setDeviceDefault();
   }
 
   void _getLocationPermission() async {
