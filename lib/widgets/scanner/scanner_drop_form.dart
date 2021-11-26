81b5cfc1964cc9c9b99414d98afdf02e1474574f
JohannesKroneisen <74152934+JohannesKroneisen@users.noreply.github.com>
2021-11-26 16:16:26 +0100

    fixed smartRefreshers for return and borrow pages

--- lib/widgets/scanner/scanner_drop_form.dart
+++ lib/widgets/scanner/scanner_drop_form.dart
@@ -1,8 +1,13 @@
+import 'package:digitaler_buecherschrank/api/api_service.dart';
 import 'package:digitaler_buecherschrank/generated/l10n.dart';
+import 'package:digitaler_buecherschrank/models/book.dart';
 import 'package:digitaler_buecherschrank/models/book_case.dart';
+import 'package:digitaler_buecherschrank/utils/shared_preferences.dart';
+import 'package:digitaler_buecherschrank/widgets/scanner/modules/popUps.dart';
 import 'package:flutter/material.dart';
-
-import 'modules/getInventoryWidget.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:quiver/core.dart';
 
 class ScannerDropForm extends StatefulWidget {
   final BookCase bookcase;
@@ -15,8 +20,27 @@ class ScannerDropForm extends StatefulWidget {
 
 class _ScannerDropFormState extends State<ScannerDropForm> {
   BookCase bookcase;
+  ApiService apiService = new ApiService();
+  Map<String, List<Book>> itemsUser = {"": []};
 
+  RefreshController _refreshControllerBorrowed =
+      RefreshController(initialRefresh: false);
   _ScannerDropFormState(this.bookcase);
+
+  void onRefreshUser(int? index) async {
+    // monitor network fetch
+    if (index != null) {
+      setState(() {
+        itemsUser["borrowed"]!.removeAt(index);
+      });
+    }
+    var i = await ApiService().getUserInventory(SharedPrefs().user);
+    setState(() {
+      itemsUser = i;
+    });
+
+    _refreshControllerBorrowed.refreshCompleted();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +51,94 @@ class _ScannerDropFormState extends State<ScannerDropForm> {
       ),
       body: SingleChildScrollView(
         child: new Container(
-          child: FutureBuilder(
-              future: getUserInventoryWidget(bookcase, context),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.92,
+            child: FutureBuilder(
+              future: apiService.getUserInventory(SharedPrefs().user),
               builder: (BuildContext context, AsyncSnapshot snapshot) {
                 if (snapshot.data == null) {
-                  return Text("");
+                  return Center(
+                    child: SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                  );
+                } else if (snapshot.data["borrowed"].isEmpty) {
+                  return Container(
+                    height: 50,
+                    child: ListTile(
+                      title: Text(S.of(context).label_empty_user,
+                          style: Theme.of(context).textTheme.bodyText1),
+                    ),
+                  );
                 } else {
-                  return snapshot.data;
+                  print(
+                      "snapshot.data['borrowed'].length: ${snapshot.data["borrowed"].length}");
+                  itemsUser = snapshot.data!;
+                  return Container(
+                    height: MediaQuery.of(context).size.height * 0.92,
+                    child: SmartRefresher(
+                      enablePullDown: true,
+                      header: ClassicHeader(),
+                      controller: _refreshControllerBorrowed,
+                      onRefresh: () {
+                        onRefreshUser(null);
+                      },
+                      child: ListView.builder(
+                        itemCount: itemsUser['borrowed']!.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return AnimationConfiguration.staggeredList(
+                            position: index,
+                            duration: const Duration(milliseconds: 375),
+                            child: SlideAnimation(
+                              verticalOffset: 50.0,
+                              child: FadeInAnimation(
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                  ),
+                                  child: ListTile(
+                                    leading: itemsUser['borrowed']![index]
+                                                .thumbnail !=
+                                            null
+                                        ? Image.network(
+                                            itemsUser['borrowed']![index]
+                                                .thumbnail!)
+                                        : Icon(Icons.book),
+                                    title: Text(
+                                        itemsUser['borrowed']![index].title!),
+                                    trailing: ElevatedButton(
+                                      child: Text(S.of(context).label_dropbook),
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder:
+                                              (BuildContext dialogContext) {
+                                            return UserPopUp(
+                                              book:
+                                                  itemsUser['borrowed']![index],
+                                              bookCase: bookcase,
+                                            );
+                                          },
+                                        ).then((value) => onRefreshUser(index));
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  );
                 }
-              }),
+              },
+            ),
+          ),
         ),
       ),
     );
--- lib/widgets/scanner/scanner_pickup_form.dart
+++ lib/widgets/scanner/scanner_pickup_form.dart
@@ -1,8 +1,10 @@
+import 'package:digitaler_buecherschrank/api/api_service.dart';
 import 'package:digitaler_buecherschrank/generated/l10n.dart';
 import 'package:digitaler_buecherschrank/models/book_case.dart';
+import 'package:digitaler_buecherschrank/widgets/scanner/modules/popUps.dart';
 import 'package:flutter/material.dart';
-
-import 'modules/getInventoryWidget.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ScannerPickupForm extends StatefulWidget {
   final BookCase bookcase;
@@ -15,8 +17,27 @@ class ScannerPickupForm extends StatefulWidget {
 
 class _ScannerPickupFormState extends State<ScannerPickupForm> {
   BookCase bookcase;
-
+  List itemsBookcase = [];
+  ApiService apiService = new ApiService();
+  RefreshController _refreshControllerBookcase =
+      RefreshController(initialRefresh: false);
   _ScannerPickupFormState(this.bookcase);
+
+  void onRefreshBookcase(String bookCaseID, int? index) async {
+    // monitor network fetch
+    print(itemsBookcase.toString());
+    if (index != null) {
+      setState(() {
+        itemsBookcase.removeAt(index);
+      });
+    }
+    List i = await ApiService().getBookCaseInventory(bookCaseID);
+    setState(() {
+      itemsBookcase = i;
+    });
+    print(itemsBookcase.toString());
+    _refreshControllerBookcase.refreshCompleted();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +48,84 @@ class _ScannerPickupFormState extends State<ScannerPickupForm> {
       ),
       body: SingleChildScrollView(
         child: new Container(
-          child: getBookCaseInventoryWidget('${bookcase.iId!.oid}', context),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.92,
+            child: FutureBuilder(
+              future: apiService.getBookCaseInventory(bookcase.iId!.oid!),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                  return Center(
+                    child: SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                  );
+                } else {
+                  print(
+                      "snapshot.data['donated'].length: ${snapshot.data.length}");
+                  itemsBookcase = snapshot.data!;
+                  return Container(
+                    height: MediaQuery.of(context).size.height * 0.92,
+                    child: SmartRefresher(
+                      enablePullDown: true,
+                      header: ClassicHeader(),
+                      controller: _refreshControllerBookcase,
+                      onRefresh: () {
+                        onRefreshBookcase(bookcase.iId!.oid!, null);
+                      },
+                      child: ListView.builder(
+                        itemCount: itemsBookcase.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return AnimationConfiguration.staggeredList(
+                            position: index,
+                            duration: const Duration(milliseconds: 375),
+                            child: SlideAnimation(
+                              verticalOffset: 50.0,
+                              child: FadeInAnimation(
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                  ),
+                                  child: ListTile(
+                                    leading:
+                                        itemsBookcase[index].thumbnail != null
+                                            ? Image.network(
+                                                itemsBookcase[index].thumbnail!)
+                                            : Icon(Icons.book),
+                                    title: Text(itemsBookcase[index].title!),
+                                    trailing: ElevatedButton(
+                                      child:
+                                          Text(S.of(context).label_borrowbook),
+                                      onPressed: () async {
+                                        showDialog(
+                                          context: context,
+                                          builder:
+                                              (BuildContext dialogContext) {
+                                            return BookcasePopUp(
+                                              book: itemsBookcase[index],
+                                              bookCaseID: bookcase.iId!.oid!,
+                                            );
+                                          },
+                                        ).then((value) => onRefreshBookcase(
+                                            bookcase.iId!.oid!, index));
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
         ),
       ),
     );
